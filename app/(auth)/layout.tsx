a9e480d8264f2eb40125859e021d10d20f47ca8b
Deepak Gariya <97430393+deep55gariya@users.noreply.github.com>
2024-05-19 22:10:02 +0530

    Update layout.tsx

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
+// import React from 'react'
 
-const Layout = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <main className="auth">{children}</main>
-  )
-}
+// const Layout = ({ children }: { children: React.ReactNode }) => {
+//   return (
+//     <main className="auth">{children}</main>
+//   )
+// }
 
-export default Layout
+// export default Layout
